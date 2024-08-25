47764857cccd770248e2c718513bae53c66f14d4
Peter Williams <retex.williams@gmail.com>
2024-08-25 09:08:58 +0100

    optimised styles.css

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ if (darkModeState !== null) {
 }
 
 // Listen for changes to the prefers-color-scheme media query
+
 prefersDarkScheme.addListener((mediaQuery) =>
   toggleDarkMode(mediaQuery.matches)
 );
@@ -40,81 +41,76 @@ document.addEventListener("DOMContentLoaded", function () {
   const emailLink = document.getElementById("email-link");
   const phoneLink = document.getElementById("phone-link");
 
-  if (emailLink) {
-    emailLink.addEventListener("click", function (event) {
-      event.preventDefault();
-      const email = this.getAttribute("href").replace("mailto:", "");
-      navigator.clipboard.writeText(email);
+  function animateCopyMessage(element, originalText) {
+    const copyText = element.classList.contains("email")
+      ? "Email Copied!"
+      : "Phone Number Copied!";
 
-      const originalText = this.textContent;
-      this.textContent = "Email Copied!";
-      gsap.fromTo(
-        this,
-        { opacity: 0, y: -20 },
-        {
-          opacity: 1,
-          y: 0,
-          duration: 0.5,
-          onComplete: () => {
-            setTimeout(() => {
-              gsap.to(this, {
-                opacity: 0,
-                y: 20,
-                duration: 0.5,
-                onComplete: () => {
-                  this.textContent = originalText;
-                  gsap.fromTo(
-                    this,
-                    { opacity: 0, y: -20 },
-                    { opacity: 1, y: 0, duration: 0.5 }
-                  );
-                },
-              });
-            }, 2000);
-          },
-        }
-      );
-    });
-  }
-
-  if (phoneLink) {
-    phoneLink.addEventListener("click", function (event) {
-      if (/Mobi|Android/i.test(navigator.userAgent)) {
-        return; // Proceed with normal behavior on mobile devices
-      } else {
-        event.preventDefault();
-        const phoneNumber = this.textContent;
-        navigator.clipboard.writeText(phoneNumber);
-
-        const originalText = this.textContent;
-        this.textContent = "Telephone Number Copied!";
+    gsap.to(element, {
+      opacity: 0,
+      y: -20,
+      duration: 0.3,
+      onComplete: () => {
+        element.textContent = copyText;
         gsap.fromTo(
-          this,
-          { opacity: 0, y: -20 },
+          element,
+          { opacity: 0, y: 20, rotation: -5, scale: 0.8 },
           {
             opacity: 1,
             y: 0,
+            rotation: 0,
+            scale: 1,
             duration: 0.5,
+            ease: "elastic.out(1, 0.5)",
             onComplete: () => {
-              setTimeout(() => {
-                gsap.to(this, {
-                  opacity: 0,
-                  y: 20,
-                  duration: 0.5,
-                  onComplete: () => {
-                    this.textContent = originalText;
-                    gsap.fromTo(
-                      this,
-                      { opacity: 0, y: -20 },
-                      { opacity: 1, y: 0, duration: 0.5 }
-                    );
-                  },
-                });
-              }, 2000);
+              gsap.to(element, {
+                opacity: 0,
+                y: 20,
+                rotation: 5,
+                scale: 0.8,
+                delay: 1.5,
+                duration: 0.3,
+                onComplete: () => {
+                  element.textContent = originalText;
+                  gsap.fromTo(
+                    element,
+                    { opacity: 0, y: -20, rotation: 0, scale: 0.8 },
+                    {
+                      opacity: 1,
+                      y: 0,
+                      scale: 1,
+                      duration: 0.3,
+                      ease: "back.out(1.7)",
+                    }
+                  );
+                },
+              });
             },
           }
         );
-      }
+      },
     });
+  }
+
+  function setupLinkAnimation(element) {
+    element.addEventListener("click", function (event) {
+      event.preventDefault();
+      const textToCopy = element.classList.contains("email")
+        ? element.getAttribute("href").replace("mailto:", "")
+        : element.textContent;
+
+      navigator.clipboard.writeText(textToCopy);
+      animateCopyMessage(element, element.textContent);
+    });
+  }
+
+  if (emailLink) {
+    emailLink.classList.add("email");
+    setupLinkAnimation(emailLink);
+  }
+
+  if (phoneLink) {
+    phoneLink.classList.add("phone");
+    setupLinkAnimation(phoneLink);
   }
 });
